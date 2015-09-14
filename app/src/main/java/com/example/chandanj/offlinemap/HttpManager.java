3cb556895100deaba5a9ab51719fa46421eaea5f
Chandan Jyoti Sharma <chandan.jyoti.sharma@intel.com>
2015-09-14 15:24:28 -0700

    Testing API

---
+++ app/src/main/java/com/example/chandanj/offlinemap/HttpManager.java
@@ -0,0 +1,87 @@
+package com.example.chandanj.offlinemap;
+
+import android.content.Entity;
+import android.net.http.AndroidHttpClient;
+import android.util.Base64;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+/**
+ * Created by cipher1729 on 8/26/2015.
+ */
+public class HttpManager {
+
+   public static String getData(String uri) {
+       try {
+           URL url = new URL(uri);
+           HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+           StringBuilder sb = new StringBuilder();
+           BufferedReader reader;
+           reader= new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+           String line = reader.readLine();
+          // line= reader.readLine();
+           while(line!= null)
+           {
+               sb.append(line+ "\n");
+               line=reader.readLine();
+           }
+           reader.close();
+           return sb.toString();
+       } catch (MalformedURLException e) {
+           e.printStackTrace();
+           return null;
+       } catch (IOException e) {
+           e.printStackTrace();
+            return null;
+       }
+       finally {
+
+       }
+
+   }
+
+
+    public static String getData(String uri, String username, String password) {
+        try {
+            URL url = new URL(uri);
+            byte[] loginBytes = (username+":"+password).getBytes();
+            StringBuilder loginBuider = new StringBuilder().append("Basic ")
+                    .append(Base64.encodeToString(loginBytes,Base64.DEFAULT));
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            httpURLConnection.setRequestProperty("Authorization",loginBuider.toString());
+            StringBuilder sb = new StringBuilder();
+            BufferedReader reader;
+            reader= new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            String line = reader.readLine();
+            // line= reader.readLine();
+            while(line!= null)
+            {
+                sb.append(line+ "\n");
+                line=reader.readLine();
+            }
+            reader.close();
+            return sb.toString();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        finally {
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/chandanj/offlinemap/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.chandanj.offlinemap;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.queryButton);
+        textView =  (TextView) findViewById(R.id.textView);
+
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                String jsonData =HttpManager.getData("https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyC9M0AmyxoajATobugixlWFd26f7kUKhkc");
+                assert(true);
+            }
+        };
+        new Thread(runnable).start();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
